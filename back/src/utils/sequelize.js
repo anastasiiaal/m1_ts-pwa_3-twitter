d239b930930d2e3dc569f159002169320d74c67c
Anastasiia A. <95031679+anastasiiaal@users.noreply.github.com>
2025-01-30 13:13:19 +0100

    fix typo

--- back/src/utils/sequelize.js
+++ back/src/utils/sequelize.js
@@ -7,9 +7,9 @@ const sequelize = new Sequelize(process.env.MYSQL_URL, {
 (async () => {
   try {
     await sequelize.authenticate();
-    // await sequelize.sync({
-    //   alter: true,
-    // })
+    await sequelize.sync({
+      // alter: true,
+    })
     .then(() => console.log("BDD synchronisée"))
     .catch(err => console.error("Erreur de synchronisation de la BDD", err));;
     console.log("Connection has been established successfully.");
