96eba139a28174099fde3f572d78bc72133155fe
Anastasiia A. <95031679+anastasiiaal@users.noreply.github.com>
2025-01-30 10:41:52 +0100

    delete all notion of comments

--- back/src/routes/commentRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const CommentController = require("../controllers/CommentController");
-
-const router = express.Router();
-
-router.post("/", CommentController.create);
-router.get("/:postId", CommentController.getAllByPost);
-
-module.exports = router;
