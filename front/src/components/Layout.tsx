3f7c2010a70b05272775566b111345c0ad463c54
Anastasiia A. <95031679+anastasiiaal@users.noreply.github.com>
2025-01-30 09:39:36 +0100

    fix typo

--- front/src/components/Layout.tsx
+++ front/src/components/Layout.tsx
@@ -4,13 +4,11 @@ export default function Layout() {
     return (
         <div className="min-h-screen bg-gray-100 flex flex-col">
             <header className="bg-slate-800 text-white py-8">
-                <div className="container">
-                    <h1 className="font-bold text-xl text-center">
-                        <Link to="/" className="text-white hover:opacity-85 ">
-                            Twitter 🕊️
-                        </Link>
-                    </h1>
-                </div>
+                <h1 className="font-bold text-xl text-center">
+                    <Link to="/" className="text-white hover:opacity-85 ">
+                        Twitter 🕊️
+                    </Link>
+                </h1>
             </header>
             <main className="flex-1 m-4">
                 <Outlet />
